491fac1fd3780996714984f4706fcb718e7a911c
Mannawar Hussain <dearmannawar@gmail.com>
2020-08-13 15:55:42 +0300

    1st

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const { request, response } = require('express')
+const app = express()
+app.use(express.json())
+
+//CORS
+const cors = require('cors')
+app.use(cors())
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+//   }
+  
+//   const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' })
+//   }
+
+//   app.use(requestLogger)
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+//Get info about all notes
+app.get('/api/notes', (request, response) => {
+    return response.send(notes)  
+})
+
+//Get info for any particular id
+app.get('/api/notes/:id', (request, response) => {
+    const id  = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).send("requested resource not found")
+    }   
+})
+
+//POST
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)): 0
+
+    return maxId + 1;
+}
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if(!body.content) {
+        return response.status(400).json(
+            {
+                error: 'content-missing'
+            }
+        )
+    }
+    const note = {
+      id: generateId(),
+      content: body.content,
+      date: new Date(),
+      important: body.important || false
+    }
+  
+    notes.concat(note)
+
+    response.json(note)
+})
+
+//Delete
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.filter(note => note.id === id)
+    response.status(204).end()
+})
+
+// app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`);
